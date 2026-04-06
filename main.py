20ea34d69f098f7ab7ea3b66c49147a44726977f
kaua <bolologordinhodocaraio123@gmail.com>
2026-04-06 10:48:43 -0300

    segunda classe criada junto com banco de dados✔

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine, Column, Integer , String, ForeignKey
 from sqlalchemy.orm import declarative_base, sessionmaker, relationship
 
-engine = create_engine("sqlite:///muitos_para_dados.db",encho=False)
+engine = create_engine("sqlite:///escola.db")
 Base = declarative_base()
 Session = sessionmaker(bind=engine)
 
@@ -11,9 +11,26 @@ class Professor(Base):
     id = Column(Integer,primary_key=True)
     nome = Column(String(100),nullable=False)
     materia = Column(String(100))
-
-    aulas = relationship("Aula", back_populates="Professor", cascade="all, delete-orphan")
+    aulas = relationship("Aula", back_populates="professor", cascade="all, delete-orphan")
 
     def __repr__(self):
         return f"<Professor(id={self.id}, nome='{self.nome}')>"
     
+class Alunos(Base):
+    __tablename__ = "alunos"
+
+    id = Column(Integer,primary_key=True)
+    nome = Column(String(100),nullable=False)
+    RA = Column(Integer)
+    idade = Column(Integer)
+
+    professor_id = Column(Integer, ForeignKey("professor.id"), nullable=False)
+
+    professor =relationship("Professor", back_populates="aulas")
+
+
+    def __repr__(self):
+        return f"<Materias(nome='{self.nome}',professor_id={self.professor_id},aulas_id={self.aulas_id})>"
+    
+Base.metadata.create_all(engine)
+
